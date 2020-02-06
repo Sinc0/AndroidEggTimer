f9c6db481c1858ca63731c37a35e30be6f686b58
Sinc0 <sinco.developer@gmail.com>
2020-02-06 23:49:24 +0100

    Added more logic, buttons and background image

--- app/src/main/java/android/example/eggtimer/MainActivity.java
+++ app/src/main/java/android/example/eggtimer/MainActivity.java
@@ -1,34 +1,185 @@
 package android.example.eggtimer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textViewCounter;
+    SeekBar seekBarAlarmTime;
+    Button buttonStartAlarm;
+    Boolean counterIsActive = false;
+    CountDownTimer timer;
 
-    public void onClickStartAlarm(View view) {
-        CountDownTimer Timer = new CountDownTimer(10000, 1000)
+    public void resetTimer()
+    {
+        textViewCounter.setText("0:00");
+        seekBarAlarmTime.setEnabled(true);
+        timer.cancel();
+        seekBarAlarmTime.setProgress(0);
+        buttonStartAlarm.setText("Start");
+        counterIsActive = false;
+
+    }
+
+    public void onClickStartAlarm(View view)
+    {
+        if (counterIsActive == true)
         {
-            @Override
-            public void onTick(long millisUntilFinished) {
 
-            }
+            resetTimer();
 
-            @Override
-            public void onFinish() {
+        }
 
-            }
-        }.start();
+        else {
+            counterIsActive = true;
+            seekBarAlarmTime.setEnabled(false);
+            buttonStartAlarm.setText("Stop");
+
+            timer = new CountDownTimer(seekBarAlarmTime.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.alarmsound);
+                    mp.start();
+                    resetTimer();
+                    Log.i("Tag:", "Alarm Finished");
+
+                }
+            };
+
+            timer.start();
+        }
+    }
+
+    public void onClickStartAlarmRare(View view)
+    {
+        if (counterIsActive == true)
+        {
+
+            resetTimer();
+
+        }
+
+        else {
+            counterIsActive = true;
+            seekBarAlarmTime.setEnabled(false);
+            buttonStartAlarm.setText("Stop");
+            seekBarAlarmTime.setProgress(180);
+
+            timer = new CountDownTimer(seekBarAlarmTime.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.alarmsound);
+                    mp.start();
+                    resetTimer();
+                    Log.i("Tag:", "Alarm Finished");
+
+                }
+            };
+
+            timer.start();
+        }
+    }
+
+    public void onClickStartAlarmMedium(View view)
+    {
+        if (counterIsActive == true)
+        {
+
+            resetTimer();
+
+        }
+
+        else {
+            counterIsActive = true;
+            seekBarAlarmTime.setEnabled(false);
+            buttonStartAlarm.setText("Stop");
+            seekBarAlarmTime.setProgress(300);
+
+            timer = new CountDownTimer(seekBarAlarmTime.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.alarmsound);
+                    mp.start();
+                    resetTimer();
+                    Log.i("Tag:", "Alarm Finished");
+
+                }
+            };
+
+            timer.start();
+        }
+    }
+
+    public void onClickStartAlarmWellDone(View view)
+    {
+        if (counterIsActive == true)
+        {
+
+            resetTimer();
+
+        }
+
+        else {
+            counterIsActive = true;
+            seekBarAlarmTime.setEnabled(false);
+            buttonStartAlarm.setText("Stop");
+            seekBarAlarmTime.setProgress(480);
+
+            timer = new CountDownTimer(seekBarAlarmTime.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.alarmsound);
+                    mp.start();
+                    resetTimer();
+                    Log.i("Tag:", "Alarm Finished");
+
+                }
+            };
+
+            timer.start();
+        }
     }
 
     public void updateTimer(int secondsLeft)
@@ -44,8 +195,10 @@ public class MainActivity extends AppCompatActivity {
             s = "0" + s;
         }
 
+
         textViewCounter.setText(m + ":" + s);
 
+        seekBarAlarmTime.setProgress(secondsLeft);
     }
 
     @Override
@@ -56,13 +209,13 @@ public class MainActivity extends AppCompatActivity {
         getWindow().getDecorView().setBackgroundColor(Color.WHITE);
 
         //Element variables
-        SeekBar seekBarAlarmTime = findViewById(R.id.seekBarAlarmTime);
-        TextView textViewCounter = findViewById(R.id.textViewCounter);
-        Button buttonStartAlarm = findViewById(R.id.buttonStartAlarm);
+        seekBarAlarmTime = findViewById(R.id.seekBarAlarmTime);
+        textViewCounter = findViewById(R.id.textViewCounter);
+        buttonStartAlarm = findViewById(R.id.buttonStartAlarm);
 
         //SeekBar settings
         seekBarAlarmTime.setMax(600);
-        seekBarAlarmTime.setProgress(60);
+        seekBarAlarmTime.setProgress(0);
 
         seekBarAlarmTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
